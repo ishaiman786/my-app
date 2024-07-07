1606a7f5d7fbce66b5d2adfaf878a8d1e1a56a0b
isha iman <isha77136@gmail.com>
2024-07-07 22:41:33 +0500

    Todo List

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,39 @@
-import React from 'react';
+
+
+
+
+import React, { useState } from 'react';
 import './NavbarStyle.css';
+
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
         <img src="logo2.png" alt="Logo" />
       </div>
-      <ul className="nav-links">
+      <ul className={`nav-links ${showMenu ? 'show' : ''}`}>
         <li>
-          <a href="#todos">Todos</a>
+          <a href="#todos" onClick={toggleMenu}>Todos</a>
         </li>
         <li>
-          <a href="#pending">Pending</a>
+          <a href="#pending" onClick={toggleMenu}>Pending</a>
         </li>
         <li>
-          <a href="#complete">Completed</a>
+          <a href="#complete" onClick={toggleMenu}>Completed</a>
         </li>
       </ul>
+      <div className={`menu-icon ${showMenu ? 'hide' : ''}`} onClick={toggleMenu}>
+        <i className="fas fa-bars"></i> {/* Replace with your menu icon */}
+      </div>
+      <div className={`close-icon ${showMenu ? '' : 'hide'}`} onClick={toggleMenu}>
+        <i className="fas fa-times"></i> {/* Replace with your close icon (X) */}
+      </div>
     </nav>
   );
 };
